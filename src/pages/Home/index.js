ab82cf184d8aa73d1a6274eb5a39bba158f7df3f
Augusto <augustow.dantas@gmail.com>
2024-06-11 18:13:19 -0300

    Marcação no mapa Home OK

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Marker } from 'react-native-maps';
 import { StatusBar } from 'expo-status-bar';
 import { 
    Background, 
@@ -9,24 +10,57 @@ import {
    BorderLogo, 
    BtnText 
 } from './styles';
+import axios from 'axios';
 import { useNavigation } from '@react-navigation/native';
 import Feather from 'react-native-vector-icons/Feather';
 
 
 export default function Home(){
-
    const navigation = useNavigation();
+   const [riskPointLocations, setRiskPointLocations] = useState([])
+
+   const fetchRiskPoints = async () => {
+      console.log('Entrou no fetch para buscar locations')
+
+      try {
+         const response = await axios.get('http://192.168.1.9:3333/getlocations'); 
+         setRiskPointLocations(response.data);
+
+         console.log('RiskPointLocations: ', response.data)
+
+      } catch (error) {
+         console.error('Erro ao buscar pontos de risco:', error);
+
+      }
+
+   };
+
+   useEffect(() => {
+      fetchRiskPoints();
+      
+   }, []);
 
    return (
       <Background>
          <StatusBar backgroundColor='#CCCCCC' />
          <StyledMapView
             initialRegion={{
-               latitude:-8.3768666,
-               longitude:-35.0109399,
+               latitude:-8.1238872,
+               longitude:-34.9075868,
                latitudeDelta:0.02,
                longitudeDelta:0.02,
-            }}/>
+            }} >
+            {riskPointLocations.map((loc) => (
+               <Marker
+                  key={loc._id}
+                  coordinate={{
+                     latitude: loc.location.latitude,
+                     longitude: loc.location.longitude,
+                  }}
+               />
+            ))}
+
+         </StyledMapView>
 
          <BorderTab>
             <Btn
